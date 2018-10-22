476fb0a6186805c87d01875f4e33c6632045e497
user147535 <user147535@92-63-141-200.rdns.melbourne.co.uk>
2018-10-22 08:06:00 -0700

    Create Split View

---
+++ Advanced UI/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Advanced UI
+//
+//  Created by user147535 on 10/22/18.
+//  Copyright © 2018 agh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
